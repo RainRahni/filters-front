9708db8f8163e63b26f4a1a2d1df1437a3085bd0
rainr <rain.rahni@mail.ee>
2024-06-06 20:12:55 +0300

    add all criteria rows are same length, add removing criterias works

--- filters/src/app/criteria-row/criteria-row.component.ts
+++ filters/src/app/criteria-row/criteria-row.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {NgForOf, NgIf} from "@angular/common";
 
@@ -16,18 +16,23 @@ import {NgForOf, NgIf} from "@angular/common";
 export class CriteriaRowComponent implements OnInit {
   criteriaForm: FormGroup = this.formBuilder.group({
     criteriaType: 'Amount',
-    criteriaCondition: 'From',
+    criteriaCondition: '',
     criteriaMetric: '',
     day: '',
     month: '',
     year: ''
   });
+  @Output() remove = new EventEmitter<void>();
+
   days: number[] = Array(31).fill(0).map((x,i)=>i+1);
   months: number[] = Array(12).fill(0).map((x,i)=>i+1);
   years: number[] = Array.from({length: 100}, (_, i) => new Date().getFullYear() - i);
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    // Additional initialization if needed
   }
+  onRemove() {
+    this.remove.emit();
+  }
+
 }
--- filters/src/app/filter-dialog/filter-dialog.component.ts
+++ filters/src/app/filter-dialog/filter-dialog.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CriteriaRowComponent } from '../criteria-row/criteria-row.component';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-filter-dialog',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    CriteriaRowComponent
+    CriteriaRowComponent,
+    NgForOf
   ],
   templateUrl: './filter-dialog.component.html',
   styleUrls: ['./filter-dialog.component.css']
 })
 export class FilterDialogComponent implements OnInit {
-  @ViewChild('rowContainer', { read: ViewContainerRef }) container!: ViewContainerRef;
 
-  constructor() { }
-
-  addRow() {
-    this.container.createComponent(CriteriaRowComponent);
-  }
+  criteria: FormGroup[] = [];
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void { }
+
+  addRow() {
+    this.criteria.push(this.createCriteriaForm());
+  }
+
+  removeRow(index: number) {
+    this.criteria.splice(index, 1);
+  }
+
+  private createCriteriaForm() {
+    return this.formBuilder.group({
+      criteriaType: 'Amount',
+      criteriaCondition: 'From',
+      criteriaMetric: '',
+      day: '',
+      month: '',
+      year: ''
+    });
+  }
 }
