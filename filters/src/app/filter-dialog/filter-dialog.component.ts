bc3101a98cb89f02f6ee297a81bd231b84498ede
rainr <rain.rahni@mail.ee>
2024-06-06 17:03:01 +0300

    add row adding functionality

--- filters/src/app/filter-dialog/filter-dialog.component.ts
+++ filters/src/app/filter-dialog/filter-dialog.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {CriteriaRowComponent} from "../criteria-row/criteria-row.component";
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CriteriaRowComponent } from '../criteria-row/criteria-row.component';
 
 @Component({
   selector: 'app-filter-dialog',
@@ -10,38 +10,16 @@ import {CriteriaRowComponent} from "../criteria-row/criteria-row.component";
     CriteriaRowComponent
   ],
   templateUrl: './filter-dialog.component.html',
-  styleUrl: './filter-dialog.component.css'
+  styleUrls: ['./filter-dialog.component.css']
 })
 export class FilterDialogComponent implements OnInit {
+  @ViewChild('rowContainer', { read: ViewContainerRef }) container!: ViewContainerRef;
 
-  filterForm: FormGroup = this.formBuilder.group({
-    filterName: '',
-    filterForm: this.formBuilder.array([])
-  });
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  createRow(): FormGroup {
-    return this.formBuilder.group({
-      criteriaType: 'AMOUNT',
-      criteriaCondition: 'More',
-      criteriaMetric: ''
-    });
-  }
+  constructor() { }
 
   addRow() {
-    const control = <FormArray>this.filterForm.controls['filterForm'];
-    control.push(this.createRow());
+    this.container.createComponent(CriteriaRowComponent);
   }
 
-  ngOnInit(): void {
-    this.addRow(); // Add initial row
-    console.log(this.filterForm.value);
-  }
-
-  onSubmit(): void {
-    if (this.filterForm) {
-      console.log(this.filterForm.value);
-    }
-  }
+  ngOnInit(): void { }
 }
